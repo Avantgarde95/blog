8c1498ef264353dc4add76babb2774370924c6ff
Avantgarde95 <95phm@kaist.ac.kr>
2020-09-21 03:28:11 +0900

    Use disqus-react & Combine with react-router

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,8 +2,10 @@
 
 import './Polyfill';
 
-import {jsx} from '@emotion/core';
+import {Global, jsx} from '@emotion/core';
 import {render} from 'react-dom';
+import {DiscussionEmbed} from 'disqus-react';
+import {BrowserRouter, useRoutes} from 'react-router-dom';
 
 require('highlight.js/styles/monokai-sublime');
 
@@ -14,12 +16,32 @@ const Article = ({html = ''}) => (
     <div dangerouslySetInnerHTML={{__html: html}}/>
 );
 
+const AppRoutes = (
+    {routes = [] as { title: string, path: string, html: string }[]}
+) => useRoutes(routes.map(({title, path, html}, index) => (
+    {
+        path: path, element: (
+            <div>
+                <Article html={html}/>
+                <DiscussionEmbed shortname={title} config={{
+                    url: path,
+                    identifier: path,
+                    title: title
+                }}/>
+            </div>
+        )
+    }
+)));
+
 const App = () => (
-    <div>
-        <Article html={Welcome}/>
-        <Article html={Test}/>
-        <div id={'disqus_thread'}/>
-    </div>
+    <BrowserRouter>
+        <Global styles={{}}/>
+        <AppRoutes routes={[
+            {title: 'Welcome', path: '/', html: Welcome},
+            {title: 'Test', path: '/test', html: Test}
+        ]}/>
+        <DiscussionEmbed shortname={'Welcome'} config={{}}/>
+    </BrowserRouter>
 );
 
 const temporaryElements = document.getElementsByClassName('Temporary');
